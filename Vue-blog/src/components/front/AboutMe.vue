a02dd9b049276ad80d28f25cb214cc3887d72b6c
Abraham Vong <73206726+AbrahamTemple@users.noreply.github.com>
2021-08-15 23:12:42 +0800

    Add files via upload

---
+++ Vue-blog/src/components/front/AboutMe.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">
+        <h2>博客概述</h2>
+        <p>作者：Moskid</p>
+        <p>站点存活：1年</p>
+        <p>主推领域：后端、大数据、渗透、算法</p>
+        <p>理念：编程永无止境</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AboutMe"
+    }
+</script>
+
+<style scoped>
+
+</style>
